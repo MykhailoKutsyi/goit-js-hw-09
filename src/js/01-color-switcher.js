8aa7fcf703bc4f108e49918bdd7385b82fc2f006
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-02-19 17:57:14 +0200

    doneTask1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+document.querySelector('button[data-start]').style.margin = "15% 0 0 40%";
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStopClick);
+
+stopButton.disabled = true;
+
+function onStartClick() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    changeBackground();
+}
+
+function changeBackground() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStopClick() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
